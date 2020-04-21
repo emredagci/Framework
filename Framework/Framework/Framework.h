bf95a60fb6a870a8bc987ac8a2077d9fc1d5affa
Emre Dağcı <admin@bitbucket.org>
2020-04-21 14:22:07 +0300

    Initial commit

---
+++ Framework/Framework/Framework.h
@@ -0,0 +1,19 @@
+//
+//  Framework.h
+//  Framework
+//
+//  Created by Emre Dağcı on 21.04.2020.
+//  Copyright © 2020 EMAKINA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Framework.
+FOUNDATION_EXPORT double FrameworkVersionNumber;
+
+//! Project version string for Framework.
+FOUNDATION_EXPORT const unsigned char FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framework/PublicHeader.h>
+
+
---
+++ Framework/Framework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  Framework
+//
+//  Created by Emre Dağcı on 21.04.2020.
+//  Copyright © 2020 EMAKINA. All rights reserved.
+//
+
+import UIKit
+
+public class Service {
+
+    private init() {}
+    
+    public static func print() -> String {
+        return "Test"
+    }
+}
